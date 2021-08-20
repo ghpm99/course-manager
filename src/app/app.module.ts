b0a0044f921b907b3dceafc867c84a2cd4434156
Guilherme Henrique <ghpm99@hotmail.com>
2021-08-20 00:36:25 -0300

    Cria tabela de cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,43 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'courses/info/:id',
+        component: CourseInfoComponent
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: '**',
+        component: Error404Component,
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
